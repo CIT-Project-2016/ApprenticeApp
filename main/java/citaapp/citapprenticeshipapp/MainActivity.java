5edff332fa877ccdc638e8a520db58eb50c3d27e
CIT200534 <mikkosarreal@gmail.com>
2016-10-21 11:59:33 +1100

    Latest working build Friday
    Everything we've done so far on 21/10/2016
    
    includes working view /edit info and added initiate contract pages
    
    Need to implement saving function for radio buttons and date pickers

---
+++ main/java/citaapp/citapprenticeshipapp/MainActivity.java
@@ -0,0 +1,36 @@
+package citaapp.citapprenticeshipapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnTimeline = (Button) findViewById(R.id.btnTimeline);
+        btnTimeline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TimelineMain.class));
+
+
+            }
+        });
+
+
+        Button btnInfo = (Button) findViewById(R.id.btnInfo);
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ViewInformation.class));
+            }
+        });
+    }
+}
