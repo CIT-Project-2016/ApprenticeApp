64707ed1861b75331fc5b0dd682af6dbc56cac9a
DonaldGit <dcameron22@gmail.com>
2016-11-01 23:00:57 +1100

    Trying out some database connectivity
    Just starting to test out some database stuff using this branch as a
    sandbox.

---
+++ java/citaapp/citapprenticeshipapp/TestDatabaseConnection.java
@@ -0,0 +1,13 @@
+package citaapp.citapprenticeshipapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestDatabaseConnection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_database_connection);
+    }
+}
