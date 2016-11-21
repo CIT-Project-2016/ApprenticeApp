6e18852dce21083ca34a35f98e330ba7c2cb1e25
DonaldGit <dcameron22@gmail.com>
2016-11-21 12:27:58 +1100

    manually updating EditInformation.java
    changed it manually to the version from feature-database-connectivity

--- java/citaapp/citapprenticeshipapp/EditInformation.java
+++ java/citaapp/citapprenticeshipapp/EditInformation.java
@@ -5,141 +5,38 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.Calendar;
 
 public class EditInformation extends AppCompatActivity {
 
-    EditText txtUsi, txtCitNum, txtAnpName, txtAnpPhone, txtAnpEmail, txtDeptName, txtDeptPhone, txtDeptEmail, txtLlnDate, txtClassDate;
+    EditText txtUsi, txtCitNum, txtAnpName, txtAnpPhone, txtAnpEmail,  txtLlnDate, txtClassDate;
+    //EditText txtDeptName, txtDeptPhone, txtDeptEmail;
 
     RadioButton rbtRPLYes, rbtTrainingYes, rbtRPLNo, rbtTrainingNo;
 
-
     DatePickerDialog datePickerDialog;
 
-    Button saveBtn, cancelBtn;
+    Button btnSave, btnCancel;
 
+    Spinner spnDeptName;
+
+    TextView lblDeptPhone, lblDeptEmail;
+
+    DBHandler dbHandler;
 
     SharedPreferences preferenceSettings;
     SharedPreferences.Editor preferenceEditor;
-
-    //separated the putString code into this separate method
-    //takes the key and value strings and puts into Shared Preferences file
-    private void saveToPrefs(String key, String value)
-    {
-        preferenceSettings = getSharedPreferences("myInfo",Context.MODE_PRIVATE);
-        preferenceEditor = preferenceSettings.edit();
-
-        preferenceEditor.putString(key, value);
-        preferenceEditor.apply();
-    }
-
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.rbtTrainingYes:
-                if (checked)
-
-                    break;
-            case R.id.rbtTrainingNo:
-                if (checked)
-
-                    break;
-        }
-    }
-
-
-
-    private void saveAllPrefs()
-    {
-        try
-        {
-            saveToPrefs("USI",  txtUsi.getText().toString());
-            saveToPrefs("CIT Number",  txtCitNum.getText().toString());
-            saveToPrefs("ANP Name",  txtAnpName.getText().toString());
-            saveToPrefs("ANP Phone",  txtAnpPhone.getText().toString());
-            saveToPrefs("ANP Email",  txtAnpEmail.getText().toString());
-            saveToPrefs("Department Name",  txtDeptName.getText().toString());
-            saveToPrefs("Department Phone",  txtDeptPhone.getText().toString());
-            saveToPrefs("Department Email",  txtDeptEmail.getText().toString());
-            saveToPrefs("LLN Date", txtLlnDate.getText().toString());
-            saveToPrefs("Class Start Date", txtClassDate.getText().toString());
-            if(rbtRPLYes.isChecked()){
-                saveToPrefs("RPL", "Yes");
-            }
-            else{
-                saveToPrefs("RPL", "No");
-            }
-            if(rbtTrainingYes.isChecked()){
-                saveToPrefs("Training Plan", "Yes");
-            }
-            else{
-                saveToPrefs("Training Plan", "No");
-            }
-
-
-            Toast.makeText(getApplicationContext(), "Your information has been saved!", Toast.LENGTH_LONG).show();
-        }
-        catch (Exception e)
-        {
-            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-    }
-
-    //just separated these into this separate method for cleanliness..
-    //just setting the text fields to the current values saved
-    private void loadPrefs()
-    {
-        preferenceSettings = getSharedPreferences("myInfo",Context.MODE_PRIVATE);
-        preferenceEditor = preferenceSettings.edit();
-
-        try {
-            txtUsi.setText(preferenceSettings.getString("USI", "Not Found"));
-            txtCitNum.setText(preferenceSettings.getString("CIT Number", "Not Found"));
-            txtAnpName.setText(preferenceSettings.getString("ANP Name", "Not Found"));
-            txtAnpPhone.setText(preferenceSettings.getString("ANP Phone", "Not Found"));
-            txtAnpEmail.setText(preferenceSettings.getString("ANP Email", "Not Found"));
-            txtDeptName.setText(preferenceSettings.getString("Department Name", "Not Found"));
-            txtDeptPhone.setText(preferenceSettings.getString("Department Phone", "Not Found"));
-            txtDeptEmail.setText(preferenceSettings.getString("Department Email", "Not Found"));
-            txtLlnDate.setText(preferenceSettings.getString("LLN Date", "Not Found"));
-            txtClassDate.setText(preferenceSettings.getString("Class Start Date", "Not Found"));
-            if("Yes".equals(preferenceSettings.getString("RPL","Yes"))){
-                rbtRPLYes.setChecked(true);
-            }
-            else
-            {
-                rbtRPLNo.setChecked(true);
-            }
-            if("Yes".equals(preferenceSettings.getString("Training Plan","Yes"))){
-                rbtTrainingYes.setChecked(true);
-            }
-            else
-            {
-                rbtTrainingNo.setChecked(true);
-            }
-
-        }
-        catch(Exception e)
-        {
-            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -153,9 +50,11 @@ public class EditInformation extends AppCompatActivity {
         txtAnpName = (EditText) findViewById(R.id.txtAnpName);
         txtAnpPhone = (EditText) findViewById(R.id.txtAnpPhone);
         txtAnpEmail = (EditText) findViewById(R.id.txtAnpEmail);
-        txtDeptName = (EditText) findViewById(R.id.txtDeptName);
-        txtDeptPhone = (EditText) findViewById(R.id.txtDeptPhone);
-        txtDeptEmail = (EditText) findViewById(R.id.txtDeptEmail);
+
+        spnDeptName = (Spinner) findViewById(R.id.spnDeptName);
+        lblDeptPhone = (TextView) findViewById(R.id.lblDeptPhone);
+        lblDeptEmail = (TextView) findViewById(R.id.lblDeptEmail);
+
         txtLlnDate = (EditText) findViewById(R.id.txtLlnDate);
         txtClassDate = (EditText) findViewById(R.id.txtClassDate);
         rbtRPLYes = (RadioButton) findViewById(R.id.rbtRPLYes);
@@ -163,6 +62,25 @@ public class EditInformation extends AppCompatActivity {
         rbtTrainingYes = (RadioButton) findViewById(R.id.rbtTrainingYes);
         rbtTrainingNo = (RadioButton) findViewById(R.id.rbtTrainingNo);
 
+
+        //
+        dbHandler = new DBHandler(this);
+        dbHandler.initSpinner(spnDeptName);
+
+
+        spnDeptName.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                lblDeptPhone.setText(dbHandler.getDeptsList().get(position).getPhone());
+                lblDeptEmail.setText(dbHandler.getDeptsList().get(position).getEmail());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        });
 
         txtLlnDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -188,7 +106,8 @@ public class EditInformation extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
-            txtClassDate.setOnClickListener(new View.OnClickListener() {
+
+        txtClassDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // calender class's instance and get current date , month and year from calender
@@ -213,8 +132,8 @@ public class EditInformation extends AppCompatActivity {
             }
         });
 
-        saveBtn = (Button) findViewById(R.id.btnSave);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
@@ -225,8 +144,8 @@ public class EditInformation extends AppCompatActivity {
             }
         });
 
-        cancelBtn = (Button) findViewById(R.id.btnCancel);
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
@@ -242,4 +161,125 @@ public class EditInformation extends AppCompatActivity {
         super.onStart(); //Always call the superclass method first
         loadPrefs();
     }
+
+    private void setSpinnerSelection()
+    {
+        for (CITDepartment tempDept : dbHandler.getDeptsList())
+        {
+            Log.d("testing department", tempDept.getName());
+            if(tempDept.getName().equals(preferenceSettings.getString("Department Name", "Not Found")))
+            {
+                spnDeptName.setSelection(tempDept.getId()-1);
+                Log.d("Found","This is the selection.");
+                //break;
+            }
+        }
+    }
+
+    //separated the putString code into this separate method
+    //takes the key and value strings and puts into Shared Preferences file
+    private void saveToPrefs(String key, String value)
+    {
+        if (value != null && value != "")
+        {
+            preferenceSettings = getSharedPreferences("myInfo",Context.MODE_PRIVATE);
+            preferenceEditor = preferenceSettings.edit();
+
+            preferenceEditor.putString(key, value);
+            preferenceEditor.apply();
+        }
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rbtTrainingYes:
+                if (checked)
+
+                    break;
+            case R.id.rbtTrainingNo:
+                if (checked)
+
+                    break;
+        }
+    }
+
+    private void saveAllPrefs()
+    {
+        try
+        {
+            saveToPrefs("USI",  txtUsi.getText().toString());
+            saveToPrefs("CIT Number",  txtCitNum.getText().toString());
+            saveToPrefs("ANP Name",  txtAnpName.getText().toString());
+            saveToPrefs("ANP Phone",  txtAnpPhone.getText().toString());
+            saveToPrefs("ANP Email",  txtAnpEmail.getText().toString());
+            saveToPrefs("Department Name",  spnDeptName.getSelectedItem().toString());
+            saveToPrefs("Department Phone",  lblDeptPhone.getText().toString());
+            saveToPrefs("Department Email",  lblDeptEmail.getText().toString());
+            saveToPrefs("LLN Date", txtLlnDate.getText().toString());
+            saveToPrefs("Class Start Date", txtClassDate.getText().toString());
+
+            if(rbtRPLYes.isChecked())
+                saveToPrefs("RPL", "Yes");
+            else
+                saveToPrefs("RPL", "No");
+
+            if(rbtTrainingYes.isChecked())
+                saveToPrefs("Training Plan", "Yes");
+            else
+                saveToPrefs("Training Plan", "No");
+
+            Toast.makeText(getApplicationContext(), "Your information has been saved!", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            //Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+
+    //just separated these into this separate method for cleanliness..
+    //just setting the text fields to the current values saved
+    private void loadPrefs()
+    {
+        preferenceSettings = getSharedPreferences("myInfo",Context.MODE_PRIVATE);
+        preferenceEditor = preferenceSettings.edit();
+
+        try {
+            txtUsi.setText(preferenceSettings.getString("USI", "Not Found"));
+            txtCitNum.setText(preferenceSettings.getString("CIT Number", "Not Found"));
+            txtAnpName.setText(preferenceSettings.getString("ANP Name", "Not Found"));
+            txtAnpPhone.setText(preferenceSettings.getString("ANP Phone", "Not Found"));
+            txtAnpEmail.setText(preferenceSettings.getString("ANP Email", "Not Found"));
+            //txtDeptName.setText(preferenceSettings.getString("Department Name", "Not Found"));
+            //txtDeptPhone.setText(preferenceSettings.getString("Department Phone", "Not Found"));
+            //txtDeptEmail.setText(preferenceSettings.getString("Department Email", "Not Found"));
+
+            setSpinnerSelection();
+
+            txtLlnDate.setText(preferenceSettings.getString("LLN Date", "Not Found"));
+            txtClassDate.setText(preferenceSettings.getString("Class Start Date", "Not Found"));
+
+
+            if("Yes".equals(preferenceSettings.getString("RPL","Yes")))
+                rbtRPLYes.setChecked(true);
+            else
+                rbtRPLNo.setChecked(true);
+
+            if("Yes".equals(preferenceSettings.getString("Training Plan","Yes")))
+                rbtTrainingYes.setChecked(true);
+            else
+                rbtTrainingNo.setChecked(true);
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
