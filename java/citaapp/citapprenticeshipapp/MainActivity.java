9fdd18435ec545afc51e13af9446824f1ff97cc5
CIT200534 <mikkosarreal@gmail.com>
2016-11-28 10:54:50 +1100

    Feature-TitleBar/SplashScreen-Finished
    TitleBar Finished
    SpashScreen Finished
    Icon Finished

--- java/citaapp/citapprenticeshipapp/MainActivity.java
+++ java/citaapp/citapprenticeshipapp/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setLogo(R.drawable.cit_logo);
-        
+        getSupportActionBar().setTitle("");
         btnTimeline = (Button) findViewById(R.id.btnTimeline);
 
         btnTimeline.setOnClickListener(new View.OnClickListener() {
