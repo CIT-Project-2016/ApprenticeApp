5e299bc6a0c369a490dd31333b69b9f8fedf7bf8
CIT200534 <mikkosarreal@gmail.com>
2016-11-28 10:18:30 +1100

    Feature-TitleBar/SplashScreen-Unfinished
    Finished the SplashScreen need to work on the titlebar and the design for the buttons

--- java/citaapp/citapprenticeshipapp/MainActivity.java
+++ java/citaapp/citapprenticeshipapp/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setLogo(R.drawable.cit_logo_rto);
-
+        toolbar.setLogo(R.drawable.cit_logo);
+        
         btnTimeline = (Button) findViewById(R.id.btnTimeline);
 
         btnTimeline.setOnClickListener(new View.OnClickListener() {
